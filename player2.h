95ea991ead3084d474a8c9ca7b49b610308bad69
Darthveloper21 <62832264+Darthveloper21@users.noreply.github.com>
2020-04-11 11:02:12 +0700

    Update player2.h

--- player2.h
+++ player2.h
@@ -12,7 +12,6 @@ class player2
 private:
     SDL_Rect cropRect;
     SDL_Rect bombRect;
-    SDL_Texture* texture;
     float frame_counter;
     int frame_width, frame_height;
     int texture_width, texture_height;
@@ -20,11 +19,11 @@ private:
     SDL_Scancode keys[6];
 
     // bomb value
-    int bombtime[100] = {0};
-    int initbomb[100] = {0};
+    int bombtime[10] = {0};
+    int initbomb[10] = {0};
     int waittime = 1500;
-    bool explode[100] = {false};
-    int bx[100] = {0}, by[100] = {0}, pocket = 1, press_cnt = 0;
+    bool explode[10] = {false};
+    int bx[10] = {0}, by[10] = {0}, pocket = 1, press_cnt = 0;
 
     //sound
     Mix_Chunk* setbomb = Mix_LoadWAV("game resources/setbomb.wav");
@@ -32,11 +31,11 @@ private:
     Mix_Chunk* pop = Mix_LoadWAV("game resources/pop.wav");
 
 public:
-    player2(SDL_Renderer* renderer, string filepath, int x, int y, int framex, int framey);
+    player2(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y, int framex, int framey);
     virtual ~player2();
 
     void update2(float delta, const Uint8 *keyState, char status_map[][100], char bomb_map[][100], char power_map[][100], SDL_Event keyboard);
-    void draw2(SDL_Renderer* renderer);
+    void draw2(SDL_Renderer* renderer, SDL_Texture* texture);
     SDL_Rect position_rect2;
     float move_speed2;
     int bomb_length2 = 1;
